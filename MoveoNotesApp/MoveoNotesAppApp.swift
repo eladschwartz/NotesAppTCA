9c73ea5183c7da566472451c1fe6ba947b6eb212
Elad Shwartz <eladshw@gmail.com>
2025-07-01 12:16:05 +0300

    Initial Commit

---
+++ MoveoNotesApp/MoveoNotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoveoNotesAppApp.swift
+//  MoveoNotesApp
+//
+//  Created by elad on 01/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoveoNotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoveoNotesAppTests/MoveoNotesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoveoNotesAppTests.swift
+//  MoveoNotesAppTests
+//
+//  Created by elad on 01/07/2025.
+//
+
+import Testing
+@testable import MoveoNotesApp
+
+struct MoveoNotesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
